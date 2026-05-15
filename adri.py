3903bed82a3aaecb1b29f6621d0baf03027d6366
Surfeur974 <surfeur974@gmail.com>
2026-05-15 16:26:45 +0200

    clean design tentative for 2stage and folded cascode

--- adri.py
+++ adri.py
@@ -424,3 +424,143 @@ def folded_cascode(nmoslk, pmoslk, s, d): ##Version Matriced
         'FP2 / FU1' : d['fp2'] / (wu1 * 1/(2*np.pi))
     }
     return m1,m2,m3,m4,m5,p
+
+def two_stage_sr_opti(nmos, pmos, s, d):
+    kBT = 1.3806488e-23 * 300
+    beta_arr     = np.atleast_1d(d['beta'])
+    cltot_cc_arr = np.atleast_1d(d['cltot_cc'])
+    N = len(beta_arr)   # axe 0
+    M = len(cltot_cc_arr)  # axe 1
+    m1 = {k: np.full((N, M), np.nan) for k in ['W','gm','gmid','cgg','cgd','cdd','ft','id']}
+    m2 = {k: np.full((N, M), np.nan) for k in ['W','gm','gmid','cgg','cgd','cdd','ft','id','cgs','fts']}
+    m3 = {k: np.full((N, M), np.nan) for k in ['W','gmid','cdd']}
+    m4 = {k: np.full((N, M), np.nan) for k in ['W','gmid','cdd']}
+    p  = {k: np.full((N, M), np.nan) for k in ['cc','cltot','cf','cs','cl','c1','rz','cn','rself1','rself2','cc_add']}
+    m1['L'] = d['L1']
+    m2['L'] = d['L2']
+    m3['L'] = d['L3']
+    m4['L'] = d['L4']
+
+    for j, beta in enumerate(beta_arr):  # j = indice beta → ligne [j,:]
+        cc    = (2/beta * kBT * d['gam1'] * (1 + d['gam3']/d['gam1'] * d['gm3_gm1'])
+               + 1/cltot_cc_arr * kBT * (1 + d['gam2'] * (1 + d['gam4']/d['gam2'] * d['gm4_gm2']))
+               ) / s['vod_noise']**2
+        cltot = cc * cltot_cc_arr
+        cf    = cltot / (1 + d['rself_2']) / (1 - beta + s['FO'] * s['G'])
+        cs    = cf * s['G']
+        cl    = cs * s['FO']
+        cgs2  = cc * d['cgs2_cc']
+        c1    = cgs2 * (1 + d['rself_1'])
+        gmR    = np.sqrt(s['L0'] / beta)
+        
+        gm_id_test = d['gm_id_test'] 
+        N_iter_gm_id_test = len(d['gm_id_test'])
+
+        X = np.zeros((N_iter_gm_id_test,M))
+    
+        wu1 = np.zeros((N_iter_gm_id_test,M))
+        gm1 = np.zeros((N_iter_gm_id_test,M))
+        cgg1 = np.zeros((N_iter_gm_id_test,M))
+        ft1 = np.zeros((N_iter_gm_id_test,M))
+        #gmid1 = np.zeros((N_iter_gm_id_test,M))
+        id1 = np.zeros((N_iter_gm_id_test,M))
+        cgs1 = np.zeros((N_iter_gm_id_test,M))
+        cin = np.zeros((N_iter_gm_id_test,M))
+        beta_actual = np.zeros((N_iter_gm_id_test,M))
+
+        for k in range(len(gm_id_test)):
+            
+            X[k,:] = s['vod_final'] * beta / 2 * gm_id_test[k]
+            X[k, X[k,:] < 1] = 1
+            #wu1[k,:] = 1 / s['ts']*(X[k,:] - 1 + np.log(X[k,:] * 1/s['ed']))
+            wu1[k,:] = 1 / s['ts']*(X[k,:] - 1 + np.log(1/(s['ed']*X[k,:])))
+            gm1[k,:]    = (wu1[k,:] * cc / beta * (1 + (1 + c1/cc) / gmR + (1 + cltot/cc) / gmR))
+            cgg1[k,:]  =  pmos.lookup('CGG_GM', GM_ID=gm_id_test[k], L=d['L1'], WARNING=False) * gm1[k,:]
+            cgs1[k,:]  =  pmos.lookup('CGS_GM', GM_ID=gm_id_test[k], L=d['L1'], WARNING=False) * gm1[k,:]
+            #cgg1[k,:]   = cf * (1/beta - 1 - s['G'])
+            ft1[k,:]    = gm1[k,:] / (2*np.pi * cgg1[k,:])
+            #gmid1[k,:]  = pmos.lookup('GM_ID', GM_CGG=2*np.pi*ft1[k,:], L=d['L1'], WARNING=False)
+            id1[k,:]    = gm1[k,:] / gm_id_test[k]
+            ##Compute input cap for real beta
+            #cgs1[k,:] = cgg1[k,:]#gm1[k,:] / (gm1[k] / cgg1[k])
+            cin[k,:] = cgs1[k,:]
+            beta_actual[k,:] = cf / (cf + cs + cin[k,:])
+
+        #Take closest point to beta, thorw away others
+        index_min_gmid = np.argmin(np.abs(beta_actual - beta), axis=0)
+
+        cols = np.arange(M)
+
+        X       = X[index_min_gmid, cols]
+        wu1     = wu1[index_min_gmid, cols]
+        gm1     = gm1[index_min_gmid, cols]
+        cgg1    = cgg1[index_min_gmid, cols]
+        ft1     = ft1[index_min_gmid, cols]
+        gmid1   = gm_id_test[index_min_gmid]
+        id1     = id1[index_min_gmid, cols]
+        cgs1    = cgs1[index_min_gmid, cols]
+        cin     = cin[index_min_gmid, cols]
+
+        slew_pct = (X - 1) / (X - 1 + np.log(X / s['ed']))
+        #print(slew_pct)
+        #######Compute actaul beta, find closest to beta, store only this value
+
+
+
+        gm2    = 2*np.pi * s['fp2'] * c1 * (1 + cltot/cc + cltot/c1)
+        fts2   = gm2 / (2*np.pi * cgs2)
+        gmid2  = nmos.lookup('GM_ID', GM_CGS=2*np.pi*fts2, L=d['L2'], WARNING=False)
+        id2    = gm2 / gmid2
+
+        # Storage [j,:] → ligne j = beta j, colonnes = tous les cltot_cc
+        p['cc'][j,:]     = cc
+        p['cltot'][j,:]  = cltot
+        p['cf'][j,:]     = cf
+        p['cs'][j,:]     = cs
+        p['cl'][j,:]     = cl
+        p['c1'][j,:]     = c1
+        m2['cgs'][j,:]   = cgs2
+        m1['gm'][j,:]    = gm1
+        m1['cgg'][j,:]   = cgg1
+        m1['ft'][j,:]    = ft1
+        m1['gmid'][j,:]  = gmid1
+        m1['id'][j,:]    = id1
+        m2['gm'][j,:]    = gm2
+        m2['fts'][j,:]   = fts2
+        m2['gmid'][j,:]  = gmid2
+        m2['id'][j,:]    = id2
+        p['rz'][j,:]     = 1 / gm2
+
+        m3['gmid'][j,:]  = gmid1 * d['gm3_gm1']
+        m4['gmid'][j,:]  = gmid2 * d['gm4_gm2']
+        m1['W'][j,:]     = id1 / pmos.lookup('ID_W', GM_ID=gmid1, L=d['L1'], WARNING=False)
+        m2['W'][j,:]     = id2 / nmos.lookup('ID_W', GM_ID=gmid2, L=d['L2'], WARNING=False)
+        m3['W'][j,:]     = id1 / nmos.lookup('ID_W', GM_ID=m3['gmid'][j,:], L=d['L3'], WARNING=False)
+        m4['W'][j,:]     = id2 / pmos.lookup('ID_W', GM_ID=m4['gmid'][j,:], L=d['L4'], WARNING=False)
+
+        m1['cgd'][j,:]   = m1['W'][j,:] * pmos.lookup('CGD_W', GM_ID=gmid1, L=d['L1'], WARNING=False)
+        p['cn'][j,:]     = m1['cgd'][j,:]
+        m2['cgd'][j,:]   = m2['W'][j,:] * nmos.lookup('CGD_W', GM_ID=gmid2, L=d['L2'], WARNING=False)
+        p['cc_add'][j,:] = cc - m2['cgd'][j,:]
+
+        m1['cdd'][j,:]   = m1['W'][j,:] * pmos.lookup('CDD_W', GM_ID=gmid1, L=d['L1'], WARNING=False)
+        m2['cdd'][j,:]   = m2['W'][j,:] * nmos.lookup('CDD_W', GM_ID=gmid2, L=d['L2'], WARNING=False)
+        m3['cdd'][j,:]   = m3['W'][j,:] * nmos.lookup('CDD_W', GM_ID=m3['gmid'][j,:], L=d['L3'], WARNING=False)
+        m4['cdd'][j,:]   = m4['W'][j,:] * pmos.lookup('CDD_W', GM_ID=m4['gmid'][j,:], L=d['L4'], WARNING=False)
+
+        p['rself1'][j,:] = (m1['cdd'][j,:] + m3['cdd'][j,:]) / m2['cgs'][j,:]
+        p['rself2'][j,:] = ((m2['cdd'][j,:] - m2['cgd'][j,:]) + m4['cdd'][j,:]) / (cl + (1 - beta) * cf)
+        #p['rself2'][j,:] = 0
+
+        gds1 = m1['W'][j,:] * pmos.lookup('GDS_W', GM_ID = gmid1, L = d['L1'], WARNING=False)
+        gds3 = m3['W'][j,:] * nmos.lookup('GDS_W', GM_ID = m3['gmid'][j,:], L = d['L3'], WARNING=False)
+        gds2 = m2['W'][j,:] * nmos.lookup('GDS_W', GM_ID = gmid2, L = d['L2'], WARNING=False)
+        gds4 = m4['W'][j,:] * pmos.lookup('GDS_W', GM_ID = m4['gmid'][j,:], L = d['L4'], WARNING=False)
+
+        R1 = 1/(gds1 + gds3)
+        R2 = 1/(gds2 + gds4)
+        L0 = beta * gm1 * R1 * gm2 * R2
+        L0 = 20*np.log10(L0)
+        p['L0'] = L0
+
+    return m1, m2, m3, m4, p
